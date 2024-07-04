d7b43bcef21e8b865db6d8feae266f95d405b01a
Muhammad Sheryar Khan <shahryar2k3@gmail.com>
2024-07-04 21:21:12 +0500

    MVC seperation

--- src/app.js
+++ src/app.js
@@ -1,26 +1,8 @@
 import express from 'express';
-import getConnection from './config/db.js';
+import tableRoutes from './routes/tableRoutes.js';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    try {
-        const pool = await getConnection();
-        const result = await pool.request().query('SELECT * FROM Customer');
-        if (result.recordset.length > 0) {
-            res.json(result.recordset);
-            result.recordset.forEach(row => {
-                if (row.Username !== null) {
-                    console.log(row.Username);
-                }
-            });
-        } else {
-            res.send('No data found');
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-});
+app.use('/', tableRoutes);
 
 export default app;
