088d6db5d24b3b279abf9d5bd7abb876b9e652b0
Muhammad Sheryar Khan <shahryar2k3@gmail.com>
2024-08-30 23:24:36 +0500

    updated Docs for Customer

--- src/controllers/admin/product/adminProductcontroller.js
+++ src/controllers/admin/product/adminProductcontroller.js
@@ -3,47 +3,53 @@ import knex from "../../../config/knex";
 
 //! TRY CATCH
 export const addProduct = async (req, res) => {
-    //? Producttypeid = 5
-    //? SKU = null
-    //? productTag isnt used
-    //? showOnHomePage wont be used (default 0)
-    //? Available start and end date not used (null)
-    //? adminComment wont be used (null)
-    //? priceGroup wont be used (null)
-    //? productCost wont be used (0.0)
-    //? hasTierPrices always true (1)
-    //? disableBuyButton always false (0)
-    //? disableWishlistButton always false (0),
-    //? isTaxExempt always false (0)
-    //? taxCategoryId is always 1
-    //? tierPriceQuantity is always 1
+    try {
 
-    const {
-        visibleIndividually,
-        aisleLocation,
-        boxQuantity,
-        name,
-        shortDescription,
-        fullDescription,
-        barcode,
-        boxBarcode,
-        published,
-        markAsNew,
-        adminComment,
-        price,
-        oldPrice,
-        //* whichever price (1-5) is present will be inserted along respective customer role 
-        price1,
-        price2,
-        price3,
-        price4,
-        price5,
-        hasDiscountApplied,
-        allowedQuantities, //* Comma seperated values of quanitites allowed
-        orderMinimumQuantity, //* if null, default = 1
-        orderMaximumQuantity, //* if null, default = 10000
-        category, //* results can only be from /product/category/all
+        //? Producttypeid = 5
+        //? SKU = null
+        //? productTag isnt used
+        //? showOnHomePage wont be used (default 0)
+        //? Available start and end date not used (null)
+        //? adminComment wont be used (null)
+        //? priceGroup wont be used (null)
+        //? productCost wont be used (0.0)
+        //? hasTierPrices always true (1)
+        //? disableBuyButton always false (0)
+        //? disableWishlistButton always false (0),
+        //? isTaxExempt always false (0)
+        //? taxCategoryId is always 1
+        //? tierPriceQuantity is always 1
+
+        const {
+            visibleIndividually,
+            aisleLocation,
+            boxQuantity,
+            name,
+            shortDescription,
+            fullDescription,
+            barcode,
+            boxBarcode,
+            published,
+            markAsNew,
+            adminComment,
+            price,
+            oldPrice,
+            //* whichever price (1-5) is present will be inserted along respective customer role 
+            price1,
+            price2,
+            price3,
+            price4,
+            price5,
+            hasDiscountApplied,
+            allowedQuantities, //* Comma seperated values of quanitites allowed
+            orderMinimumQuantity, //* if null, default = 1
+            orderMaximumQuantity, //* if null, default = 10000
+            category, //* results can only be from /product/category/all
+
+        } = req.body;
         
-    } = req.body;
+    } catch (error) {
+
+    }
 }
 
--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -6,10 +6,113 @@ const router = express.Router();
 
 router.use(authenticateToken);
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Customer
+ *     description: Customer related endpoints
+ * /customer/info:
+ *   get:
+ *     summary: Retrieve customer information
+ *     tags: [Customer]
+ *     responses:
+ *       200:
+ *         description: Customer information retrieved successfully.
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/info', getCustomerInfo);
 
-router.put('/change-password', changePassword)
+/**
+ * @swagger
+ * tags:
+ *   - name: Customer
+ *     description: Customer related endpoints
+ * /customer/change-password:
+ *   put:
+ *     summary: Change customer password
+ *     tags: [Customer]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               currentPassword:
+ *                 type: string
+ *                 description: The current password of the customer
+ *               newPassword:
+ *                 type: string
+ *                 description: The new password for the customer
+ *     responses:
+ *       200:
+ *         description: Password changed successfully.
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.put('/change-password', changePassword);
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Customer
+ *     description: Customer related endpoints
+ * /customer/update-info:
+ *   put:
+ *     summary: Update customer information
+ *     tags: [Customer]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               FirstName:
+ *                 type: string
+ *                 description: The first name of the customer
+ *               LastName:
+ *                 type: string
+ *                 description: The last name of the customer
+ *               Company:
+ *                 type: string
+ *                 description: The company of the customer
+ *               Address1:
+ *                 type: string
+ *                 description: The address of the customer
+ *               ZipPostalCode:
+ *                 type: string
+ *                 description: The zip or postal code of the customer
+ *               City:
+ *                 type: string
+ *                 description: The city of the customer
+ *               CountryId:
+ *                 type: integer
+ *                 default: 2
+ *                 description: The country ID of the customer
+ *               StateProvinceId:
+ *                 type: integer
+ *                 description: The state or province ID of the customer
+ *               PhoneNumber:
+ *                 type: string
+ *                 description: The phone number of the customer
+ *     responses:
+ *       200:
+ *         description: Customer information updated successfully.
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/update-info', updateCustomerInfo);
 
 export default router;
