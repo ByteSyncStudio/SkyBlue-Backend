1119658e5964824f024aba34a518d725018d4fb4
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2024-09-18 05:58:46 +0500

    Shortened names for months

--- src/repositories/admin/stats/adminStatsRepositories.js
+++ src/repositories/admin/stats/adminStatsRepositories.js
@@ -349,8 +349,8 @@ export async function TotalOrdersInPastMonths() {
         .first();
 
       const monthNames = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
       ];
 
       result.push({
