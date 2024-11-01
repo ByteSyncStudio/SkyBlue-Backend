dca5e729b847072553229c1c30bf075994bcf211
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2024-11-01 23:00:14 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import app from './app.js';
 import dotenv from 'dotenv'
 
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${port}`);
 });
