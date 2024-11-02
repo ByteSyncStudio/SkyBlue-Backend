aa13c545dbb899499e2019a45e667c97f31aa515
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2024-11-03 01:49:08 +0500

    connection string

--- src/config/db.js
+++ src/config/db.js
@@ -2,19 +2,28 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const config = {
+//   client: 'mssql',
+//   connection: {
+//     server: process.env.DB_SERVER,
+//     database: process.env.DB_DATABASE,
+//     uid: process.env.DB_USER,
+//     pwd: process.env.DB_PASSWORD,
+//     options: {
+//       encrypt: true,
+//       trustServerCertificate: true,
+//       port: 1433 
+//     },
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   }
+// };
+
 const config = {
   client: 'mssql',
-  connection: {
-    server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
-    uid: process.env.DB_USER,
-    pwd: process.env.DB_PASSWORD,
-    options: {
-      encrypt: true,
-      trustServerCertificate: true,
-      port: 1433 
-    },
-  },
+  connection: process.env.DB_CONNECTION_STRING,
   pool: {
     min: 2,
     max: 10,
