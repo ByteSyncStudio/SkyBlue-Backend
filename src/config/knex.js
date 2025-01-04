0cec07716c070e5a7b62f1361ac8de9fc4280846
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2025-01-04 05:22:13 +0500

    .

--- src/config/knex.js
+++ src/config/knex.js
@@ -1,12 +1,13 @@
 import Knex from 'knex';
 import knexConfig from '../config/db.js';
+import colors from 'colors';
 
 const knex = Knex(knexConfig);
 
 // Test the connection
 knex.raw('SELECT 1')
   .then(() => {
-    console.log(`Connected to DB: ${knexConfig.connection.database} at ${knexConfig.connection.host} as ${knexConfig.connection.user}`);
+    console.log(`Connected to DB: ${knexConfig.connection.database} at ${knexConfig.connection.host} as ${knexConfig.connection.user}`.cyan.underline.bold);
   })
   .catch((err) => {
     console.error('Failed to connect to the database:', err);
